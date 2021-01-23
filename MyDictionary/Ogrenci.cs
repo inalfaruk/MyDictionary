40318ea84048b4233dfd6f3dbc8cb04e8ce57b6f
farukinal16 <farukinal16@gmail.com>
2021-01-23 20:55:17 +0300

    Add project files.

---
+++ MyDictionary/Ogrenci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+     class Ogrenci
+    {
+
+
+        Dictionary<int, string> Student = new Dictionary<int, string>();
+      
+
+        public void Add(int numara,string adsoyad)
+        {
+            Student.Add(numara, adsoyad);
+
+        }
+
+        public void List()
+        {
+
+            foreach (var item in Student)
+            {
+
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+        }
+        
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci Student = new Ogrenci();
+
+            Student.Add(244,"Faruk");
+            Student.Add(234, "Ahmet");
+            Student.Add(13, "Ayşe");
+
+         
+
+            Student.List();
+        }
+
+         
+    }
+}
